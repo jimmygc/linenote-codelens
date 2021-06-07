4d72966a3633098e62d333d45b79b39f8fd694e7
guochao <guochao@winhong.com>
2021-06-07 17:34:08 +0800

    tiny fix

--- fetch_sqlite3_cross.sh
+++ fetch_sqlite3_cross.sh
@@ -1,6 +1,6 @@
 sqlite_ver=$(npm view sqlite3 version)
-node_modules/.bin/node-pre-gyp install --target_platform=win32 --target_arch=ia32 --directory=./node_modules/sqlite3  --target=${sqlite_ver} --update-binary
-node_modules/.bin/node-pre-gyp install --target_platform=win32 --target_arch=x64 --directory=./node_modules/sqlite3  --target=${sqlite_ver} --update-binary
-node_modules/.bin/node-pre-gyp install --target_platform=darwin --target_arch=x64 --directory=./node_modules/sqlite3  --target=${sqlite_ver} --update-binary
-node_modules/.bin/node-pre-gyp install --target_platform=linux --target_arch=x64 --directory=./node_modules/sqlite3  --target=${sqlite_ver} --update-binary
+node_modules/.bin/node-pre-gyp install --target_platform=win32 --target_arch=ia32 --directory=./node_modules/sqlite3  --target=${sqlite_ver}
+node_modules/.bin/node-pre-gyp install --target_platform=win32 --target_arch=x64 --directory=./node_modules/sqlite3  --target=${sqlite_ver}
+node_modules/.bin/node-pre-gyp install --target_platform=darwin --target_arch=x64 --directory=./node_modules/sqlite3  --target=${sqlite_ver}
+node_modules/.bin/node-pre-gyp install --target_platform=linux --target_arch=x64 --directory=./node_modules/sqlite3  --target=${sqlite_ver}
 
